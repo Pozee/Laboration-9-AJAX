bb0f2bd3a766722759fb4ee2aefaf529802b289e
Pozee <55946166+Pozee@users.noreply.github.com>
2019-12-10 11:53:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const apiUrl = "https://forverkliga.se/JavaScript/api/crud.php";
 let userKey = "s7kbs";
+//s7kbs
 
-window.addEventListener("load", () => {
+window.addEventListener("load", () => { // Load
   let genKey = document.querySelector("#gen-key")
   genKey.addEventListener("click", event => { // Genererar en API nyckel när någon trycker på knappen.
     fetch(apiUrl + "?requestKey")
@@ -18,6 +19,7 @@ window.addEventListener("load", () => {
     let keyValue = document.querySelector("#key-value").value;
     userKey = keyValue;
     document.querySelector("#login-status").innerHTML = "Du är nu inloggad!";
+    getData(userKey , 4);
   })
 
   let saveBook = document.querySelector("#save-book") // Hanterar det som händer när man trycker på lägg till bok knapppen.
@@ -29,10 +31,10 @@ window.addEventListener("load", () => {
 
   let getBooks = document.querySelector("#get-book") // Hanterar det som händer när man trycker på Hämta böcker knappen.
   getBooks.addEventListener("click", event => {
-    getData(userKey)
+    getData(userKey, 4)
   })
 
-});
+}); // Load end
 
 
 
@@ -43,42 +45,36 @@ function addData(title, author, key) { // Funktion för att spara vår bok till 
     .then(data => {
       let laTillBok = document.querySelector("#la-till-bok")
       laTillBok.innerHTML = "Boken är tillagd!";
-      getData(key)
+      getData(key, 4)
       document.querySelector("#boktitel").value = "" // Tömmer text fälten efter att vi har lagt till vår bok.
       document.querySelector("#författare").value = "" // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     });
 }
 
-function getData(key) { // Funktion för att hämta alla böcker som tillhör nyckeln.
+function getData(key, triesLeft) { // Funktion för att hämta alla böcker som tillhör nyckeln.
   let url = `${apiUrl}?key=${key}&op=select`; // Lägger ihop en url med key och operation.
   let bookList = document.querySelector("#bok-lista");
   fetch(url) // Skickar vår url till servern och tar sedan hand om svaret.
     .then(response => response.json())
     .then(books => {
-      console.log(books);
-
       if (books.status === "success") { // Kollar om vi lyckades hämta alla böcker.
         let getBookStatus = document.querySelector("#get-book-status");
-        getBookStatus.innerHTML = "Operation succeeded";
+        getBookStatus.innerHTML = "Lyckades hämta data från API.";
         getBookStatus.style.color = "green";
         books.data.forEach(element => { // Loopar igenom vår array och lägger title och author i en ny li.
           let bookElement = document.createElement("li")
           bookElement.innerHTML = element.title + " av " + element.author + ".";
           bookList.appendChild(bookElement);
-
         });
-      } else if (books.status === "error") { // Om vi får tillbaka ett error skickar vi det till errorHandler som hanterar det.
-        errorHandler();
-      }
-    });
+      } else if (books.status === "error") { // Får vi tillbaka ett error hamnar vi här.
+        let getBookStatus = document.querySelector("#get-book-status");
+        getBookStatus.innerHTML = "API error.";
+        getBookStatus.style.color = "red";
+        if(triesLeft >= 1) 
+          getData(key, triesLeft-1)
+        }
+    })
 }
 
-function errorHandler() {
-  let getBookStatus = document.querySelector("#get-book-status");
-  getBookStatus.innerHTML = "Operation failed";
-  getBookStatus.style.color = "red";
-}
-
-
-      //s7kbs
+      
 
